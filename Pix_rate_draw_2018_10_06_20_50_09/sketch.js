6ce8c3c76364bf9fbcf6ae8ce6c50f2070d58b3f
SabrinaNassar <37004428+SabrinaNassar@users.noreply.github.com>
2018-10-06 13:50:55 -0700

    Add files via upload

---
+++ Pix_rate_draw_2018_10_06_20_50_09/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+  colorMode(HSB, width, height, 100);
+}
+
+function draw() {
+  for (var i=0; i<width; i++ );    
+  var x = random(width);
+  var y = random(height);
+
+  stroke(x, y, 100);
+  point(x, y);
+}
